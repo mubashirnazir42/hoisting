83344b068355bad5a42b09d5d29ac2f135b01ab6
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-23 17:47:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// function printAge() {
+//   let friendName = "Taha";
+
+//   const yasir = function () {
+//     console.log(friendName);
+//   };
+//   yasir();
+// }
+// let fname = "mubashir";
+// printAge();
+// console.log(x);
+// var x = 1;
+
+// let numlet = 2;
+// console.log(window);
+// console.log(x === 1);
+// console.log(closed);
+
+// a = 5;
+// console.log(a);
+// var a;
